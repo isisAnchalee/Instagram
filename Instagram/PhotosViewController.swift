bbd268ea6c40740106c31d80d04aca3bfe8312ff
isisanchalee <isis.wenger@onelogin.com>
2016-02-03 19:18:18 -0800

    Add cocoapods and photosviewcontroller

---
+++ Instagram/PhotosViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Isis Anchalee on 2/3/16.
+//  Copyright © 2016 Isis Anchalee. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let client_id = "e05c462ebd86446ea48a5af73769b602"
+        let url = NSURL(string: "https://api.instagram.com/v1/media/popular?client_id=\(client_id)")
+        
+        let request = NSURLRequest(URL: url!)
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate: nil,
+            delegateQueue: NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                            NSLog("response: \(responseDictionary)")
+                            self.movies = responseDictionary["results"] as! [NSDictionary]
+                            self.tableView.reloadData()
+                            
+                    }
+                }
+        })
+        
+        task.resume()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
